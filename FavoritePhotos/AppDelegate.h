6e4972593f0dbabf5a8b6dac46e99a60cf961980
jjc3737 <Jane@Jaehees-MacBook-Pro.local>
2015-07-30 14:34:17 -0700

    Initial Commit

---
+++ FavoritePhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavoritePhotos
+//
+//  Created by Jaehee Chung on 7/30/15.
+//  Copyright (c) 2015 Jaehee Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FavoritePhotos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FavoritePhotos
+//
+//  Created by Jaehee Chung on 7/30/15.
+//  Copyright (c) 2015 Jaehee Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
